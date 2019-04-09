207280dd2f24b1e39c9eb0af86f3f7c1625c5cd3
Adriana <ajimenez0024@gmail.com>
2019-04-09 22:02:48 +0000

    added new route

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
 var express = require("express");
 var app = express();
-var PORT = 8080; // default port 8080
+var PORT = 8080;
 
+app.set("view engine", "ejs");  //add ejs
+
+//creates a database of urls 
 var urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
@@ -20,4 +23,13 @@ app.get("/urls.json", (req, res) => {
 });
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+//route handler for urls
+app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+});
+app.get("/urls/:shortURL", (req, res) => {
+    let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    res.render("urls_show", templateVars);
 });
